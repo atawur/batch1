6bc9be1e76f749695ed845ec53de02b3b3219a93
atawurrahman <rahmanatawur@gmail.com>
2021-08-23 23:24:55 +0600

    added

---
+++ php/sixth_class/turnery.php
@@ -0,0 +1,36 @@
+<?php
+    $number = 2;
+
+if($number==5){
+    echo 5;
+}else{
+    echo "Not five";
+}
+
+echo "<br>";
+  echo $number == 5 ? 5:"not five"; 
+  echo   "<br>";
+
+if($number > 4){
+    if($number==5){
+        echo 5;
+
+    }else{
+        echo "not 5";
+    }
+}else{
+    echo "less than 4";
+}
+echo "<br>";
+echo $number > 4 ? ($number==5 ? 5: "Not 5") : "less than 4";
+
+
+//1 odd or even
+$number = 20;
+the number 20 is even 
+the number 21 is odd
+//2 equal then which is small and which is getter
+$v1= 23
+v3 = 25
+
+?>
---
+++ php/third_class/assignment_operator.php
@@ -0,0 +1,62 @@
+<?php
+ 
+$x = 12;
+$y = 13;
+
+ //$x = $x+$y;
+ //$x+=$y;
+//echo $x;
+
+$y+=$x; // $y=$y+$x
+
+
+//echo $y;
+//echo "<br>";
+//echo $x;
+
+
+
+$firs_number = 23;
+$seconNumber = 30;
+/*
+$firs_number+=$seconNumber; // $firs_number = $firs_number+$seconNumber
+
+echo "<br>";
+echo $firs_number;
+echo "<br>";
+///sub
+echo $firs_number;
+
+echo "<br>";
+
+
+$firs_number-=$seconNumber;//fisrtnumbe = fristnumber-secondnumber
+
+echo "<br>";
+echo $firs_number;
+*/
+
+//  Multiplication
+
+
+/*$firs_number *=$seconNumber;/// firstnumber=firstNumber*$secon
+
+
+echo $firs_number;
+*/
+
+/*
+$firs_number/=$seconNumber; //firstnumber = firstNumber/secondNumber
+echo $firs_number;
+*/
+
+//$firs_number%=$seconNumber;//
+
+
+$new = $seconNumber % $firs_number;
+
+$seconNumber%=$firs_number;
+
+echo $seconNumber ;
+
+?>
